765b51df24dfcacf7573ecc7aece55150e0c4f97
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-12-01 10:42:56 +0530

    Finding the NUmber

---
+++ arrays/FindNum.java
@@ -0,0 +1,16 @@
+public class FindNum {
+  public static void main(String[] args) {
+    int [] arr = {1,5,9,4,8,7};
+     if(find(9, arr)) System.out.println("The number is found.");
+     else System.out.println("The number is not found");
+
+  }
+  public static boolean find(int n,int [] arr){
+     for (int i : arr) {
+        if (n==i) {
+            return true;
+        }
+     }
+     return false;
+  }
+}
